9c88d50c4ab052766eba32c822d62ea9692e6520
geanyuki10 <geanexecutive@hotmail.com>
2025-09-30 20:26:59 -0300

    Delete pages/api/command.js

--- pages/api/command.js
+++
@@ -1,9 +0,0 @@
-// Este arquivo pode ser removido ou deixado vazio
-// já que não usaremos mais APIs para comandos
-
-export default async function handler(req, res) {
-  res.status(200).json({ 
-    success: true, 
-    message: 'API funcionando - Use a versão local para comandos reais' 
-  });
-}
